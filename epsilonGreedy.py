d3acd9db168afdffb83b8606a374acbf036f86ec
Evan Damron <128383534+EvanDamron@users.noreply.github.com>
2023-08-16 15:40:35 -0400

    Update epsilonGreedy.py
    made compatible with the removal of all the reward modes other than "NORMALIZED"

--- epsilonGreedy.py
+++ epsilonGreedy.py
@@ -2,30 +2,27 @@
 # violating battery-of-drone budget
 # Code Written by Evan Damron, University of Kentucky Computer Science
 
-from mapping import processSHP, findMinTravelDistance, plotPath, getSensorNames, minSetCover
-import geopandas as gpd
+from mapping import findMinTravelDistance, plotPath, getSensorNames
 import pandas as pd
 import matplotlib.pyplot as plt
-from ML import getMSE, processData
+from ML import getMSE
 import random
-from joblib import Parallel, delayed
 import time
 import numpy as np
-from algorithmHelpers import processFiles, getEnergy, getPointsInBudget, addRandomHP, remRandomHP, addBestHP, remBestHP, \
+from algorithmHelpers2 import processFiles, getEnergy, getPointsInBudget, addRandomHP, remRandomHP, addBestHP, remBestHP, \
     createMSEPlot, updateMSEPlot, printResults, writeResults, printTime
 
 
 
-def epsilonGreedy(numLoops, startTime, savePlots=False, pathPlotName="", msePlotName="", outputTextName="", droneHeight=15,
-                  addRewardMode="MSE", remRewardMode="MSE", thresholdFraction=0.95, energyWeight=0,
-                  communicationRadius=70, energyBudget=60000, joulesPerMeter=10, joulesPerSecond=35, dataSize=250,
-                  transferRate=9, minSet=False, generate=False):
+def epsilonGreedy(numLoops, startTime, savePlots=False, pathPlotName="", msePlotName="", outputTextName="",
+                  droneHeight=15, energyWeight=0, communicationRadius=70, energyBudget=60000, joulesPerMeter=10,
+                  joulesPerSecond=35, dataSize=250, transferRate=9, minSet=False, generate=False, numSensors=37):
 
-    fig1, ax1, HP_gdf, UHP_gdf, SHP_gdf, sensorNames, df = processFiles(droneHeight, communicationRadius, minSet, generate)
+    fig1, ax1, HP_gdf, UHP_gdf, SHP_gdf, sensorNames, df = processFiles(droneHeight, communicationRadius, minSet,
+                                                                        generate, numSensors)
     print(f"Total number of Hoverpoints: {len(HP_gdf)}")
     x, y, line, ax2, fig2 = createMSEPlot()
-    print(
-        f"EPSILON-GREEDY ALGORITHM WITH REWARD MODE {addRewardMode} (ADD) {remRewardMode} (REMOVE), AND ENERGY WEIGHT {energyWeight}")
+    print(f"EPSILON-GREEDY ALGORITHM WITH {numLoops} LOOPS AND ENERGY WEIGHT {energyWeight}")
     arProb = 0  # probability of adding (0) and removing (1)
     rbProb = 1  # probability of random (1) and best (0)
     loopCount = 0
@@ -53,11 +50,11 @@ def epsilonGreedy(numLoops, startTime, savePlots=False, pathPlotName="", msePlot
             UHP_gdf, SHP_gdf = remRandomHP(UHP_gdf, SHP_gdf)
         elif randomNumber < raProb + rrProb + baProb:
             UHP_gdf, SHP_gdf = addBestHP(unselected=UHP_gdf, unselectedIB=pointsInBudget, selected=SHP_gdf,
-                                         rewardMode=addRewardMode, thresholdFraction=thresholdFraction,
                                          sensorNames=sensorNames, df=df, energyWeight=energyWeight)
         else:
-            UHP_gdf, SHP_gdf = remBestHP(UHP_gdf, SHP_gdf, rewardMode=remRewardMode, sensorNames=sensorNames,
-                                         thresholdFraction=thresholdFraction, df=df, energyWeight=energyWeight, joulesPerMeter=joulesPerMeter, joulesPerSecond=joulesPerSecond, dataSize=dataSize, transferRate=transferRate)
+            UHP_gdf, SHP_gdf = remBestHP(UHP_gdf, SHP_gdf, sensorNames=sensorNames, df=df, energyWeight=energyWeight,
+                                         joulesPerMeter=joulesPerMeter, joulesPerSecond=joulesPerSecond,
+                                         dataSize=dataSize, transferRate=transferRate)
 
         UHP_gdf = UHP_gdf.reset_index(drop=True)
         SHP_gdf = SHP_gdf.reset_index(drop=True)
@@ -71,6 +68,8 @@ def epsilonGreedy(numLoops, startTime, savePlots=False, pathPlotName="", msePlot
         if SHP_gdf.empty:
             totalEnergy = 0
         else:
+            SHP_gdf, _ = getEnergy(dataSize=dataSize, joulesPerMeter=joulesPerMeter, joulesPerSecond=joulesPerSecond,
+                                   selected=SHP_gdf, sensorNames=sensorNames, transferRate=transferRate)
             totalEnergy = SHP_gdf['energy'][0]
 
         print(f"This set of hoverpoints requires {totalEnergy} Joules out of the"
@@ -84,7 +83,7 @@ def epsilonGreedy(numLoops, startTime, savePlots=False, pathPlotName="", msePlot
         if len(UHP_gdf) == 0:
             arProb = totalEnergy / energyBudget
         else:
-            arProb = (energyBudget / energyBudget) * (
+            arProb = (totalEnergy / energyBudget) * (
                     1 - (len(pointsInBudget) / len(UHP_gdf)))  # as we approach budget, more likely to remove
         if len(UHP_gdf) + len(SHP_gdf) != len(HP_gdf):
             print('ERROR: SELECTED + UNSELECTED != HP')
@@ -102,8 +101,7 @@ def epsilonGreedy(numLoops, startTime, savePlots=False, pathPlotName="", msePlot
             minEnergy = tempBestSHP['energy'][0]
             bestSHP = tempBestSHP.copy()
             distanceOfBest = distance
-    print(
-        f"EPSILON-GREEDY ALGORITHM WITH REWARD MODE {addRewardMode} (ADD) {remRewardMode} (REMOVE), AND ENERGY WEIGHT {energyWeight}")
+    print(f"EPSILON-GREEDY ALGORITHM WITH {numLoops} LOOPS AND ENERGY WEIGHT {energyWeight}")
     printResults(bestSHP, iterationOfBest, distanceOfBest, minMSE, sensorNames)
     ax2.scatter(iterationOfBest, minMSE, color='red', zorder=10)
     plotPath(ax1, bestSHP)
@@ -119,8 +117,8 @@ def epsilonGreedy(numLoops, startTime, savePlots=False, pathPlotName="", msePlot
     else:
         plt.show()
 
-np.random.seed(42)
-random.seed(42)
+rSeed = 1
+np.random.seed(rSeed)
+random.seed(rSeed)
 startTime = time.time()
-epsilonGreedy(numLoops=1, generate=True, startTime=startTime, addRewardMode='NORMALIZED', remRewardMode='NORMALIZED',
-              energyWeight=0)
+epsilonGreedy(numLoops=200, generate=True, startTime=startTime, energyWeight=0, energyBudget=50000)
