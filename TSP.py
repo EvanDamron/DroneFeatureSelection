336c1a6f6651cb0f842c6ff6067697b9ad0adcd0
Evan Damron <128383534+EvanDamron@users.noreply.github.com>
2023-11-02 11:59:45 -0400

    Add files via upload

--- TSP.py
+++ TSP.py
@@ -7,12 +7,19 @@ from shapely import Point
 
 
 
-def hoverPointsTSP(points):
+def hoverPointsTSP(points, scramble=False):
+    # print(f'points: {points}')
+    if len(points) == 0:
+        gdf = gpd.GeoDataFrame(geometry=[Point(x, y) for x, y in points], crs='EPSG:3857')
+        return gdf, 0
     depotLocation = (0, 0)
     points = np.insert(points, 0, depotLocation, axis=0)
     distanceMatrix = euclidean_distance_matrix(points)
-    initPerm = list(range(len(points)))
-    permutation, distance = solve_tsp_local_search(distance_matrix=distanceMatrix, x0=initPerm)
+    if scramble == False:
+        initPerm = list(range(len(points)))
+        permutation, distance = solve_tsp_local_search(distance_matrix=distanceMatrix, x0=initPerm)
+    else:
+        permutation, distance = solve_tsp_local_search(distance_matrix=distanceMatrix)
     orderedPoints = [points[i] for i in permutation if i != 0]
     gdf = gpd.GeoDataFrame(geometry=[Point(x, y) for x, y in orderedPoints], crs='EPSG:3857')
     return gdf, distance
